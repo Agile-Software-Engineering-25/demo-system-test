b9962ab0b38ea3f6b0dbdf2bf9da32d8b528634c
py-bay <github.com.tribunal325@passmail.net>
2025-07-10 15:46:01 +0200

    set browser headless to true

--- src/test/java/com/ase/demo/base/TestBase.java
+++ src/test/java/com/ase/demo/base/TestBase.java
@@ -27,7 +27,7 @@ public class TestBase {
 
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                .setHeadless(false));
+                .setHeadless(true));
 
         context = browser.newContext(new Browser.NewContextOptions()
                 .setViewportSize(1280, 720)
